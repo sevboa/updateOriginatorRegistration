04a1b75e3a5a3badf77332f68f04e48a21afa846
a.sevbo <a.sevbo@easy-sms.ru>
2019-03-22 11:05:06 +0300

    Release terminal interface

--- modules/OriginatorImporter.py
+++ modules/OriginatorImporter.py
@@ -6,6 +6,7 @@ from operator import attrgetter
 import openpyxl
 
 from modules.Counter import counter
+from modules.Commons import csvUnload, sortingByFieldNames
 
 from modules.originatorImport.ImporterBeeline import importerBeeline
 from modules.originatorImport.ImporterMts import importerMts
@@ -20,15 +21,18 @@ class originatorImporter:
     def __init__(self, provider, fileName):
         if   provider == 'beeline':
             self.importOriginators(importerBeeline(fileName))
+            self.export(provider)
         elif provider == 'mts':
             self.importOriginators(importerMts(fileName))
+            self.export(provider)
         elif provider == 'tele2':
             self.importOriginators(importerTele2(fileName))
+            self.export(provider)
         elif provider == 'smsc':
             self.importOriginators(importerSmsc(fileName))
+            self.export(provider)
         else:
             print(provider + ' пока не работает!')
-            pause()
             exit()
 
     def importOriginators(self, ProviderImporter):
@@ -70,10 +74,10 @@ class originatorImporter:
         Counter.lastTell('deduplicated')
         return list(originatorsDict.values())
 
-    def sqlGenerate(self, originators, count):
+    def sqlGenerate(self, originators, count, provider):
         queryString = str()
-        queryBegin = 'CREATE TABLE originators_smsc(operator_group_id integer, service_type_id integer, originator varchar, originator_change varchar, account_id text, status_id integer, contractor_legal_entity text, contractor_inn text, is_incorrect_inn integer, originator_id integer, originator_change_id integer);'
-        queryBegin2 = '\n\nINSERT INTO originators_smsc(operator_group_id, service_type_id, originator, originator_change, account_id, status_id, contractor_legal_entity, contractor_inn, is_incorrect_inn, originator_id, originator_change_id) \nVALUES \n'
+        queryBegin = 'CREATE TABLE originators_' + provider + '(operator_group_id integer, service_type_id integer, originator varchar, originator_change varchar, account_id text, status_id integer, contractor_legal_entity text, contractor_inn text, is_incorrect_inn integer, originator_id integer, originator_change_id integer);'
+        queryBegin2 = '\n\nINSERT INTO originators_' + provider + '(operator_group_id, service_type_id, originator, originator_change, account_id, status_id, contractor_legal_entity, contractor_inn, is_incorrect_inn, originator_id, originator_change_id) \nVALUES \n'
         queryMiddleList = list()
         queryMiddleString = str()
         stringCount = int()
@@ -114,10 +118,10 @@ class originatorImporter:
         print('sql generated')
         return queryString
     
-    def sqlGenerateByOperators(self, originators, count):
+    def sqlGenerateByOperators(self, originators, count, provider):
         queryList = list()
-        queryBegin = 'CREATE TABLE originators_smsc(operator_group_id integer, service_type_id integer, originator varchar, originator_change varchar, account_id text, status_id integer, contractor_legal_entity text, contractor_inn text, is_incorrect_inn integer, originator_id integer, originator_change_id integer);'
-        queryBegin += '\nINSERT INTO originators_smsc(operator_group_id, service_type_id, originator, originator_change, account_id, status_id, contractor_legal_entity, contractor_inn, is_incorrect_inn, originator_id, originator_change_id) \nVALUES \n'
+        queryBegin = 'CREATE TABLE originators_' + provider + '(operator_group_id integer, service_type_id integer, originator varchar, originator_change varchar, account_id text, status_id integer, contractor_legal_entity text, contractor_inn text, is_incorrect_inn integer, originator_id integer, originator_change_id integer);'
+        queryBegin += '\nINSERT INTO originators_' + provider + '(operator_group_id, service_type_id, originator, originator_change, account_id, status_id, contractor_legal_entity, contractor_inn, is_incorrect_inn, originator_id, originator_change_id) \nVALUES \n'
         queryMiddleList = list()
         queryMiddleString = str()
         stringCount = int()
@@ -199,3 +203,64 @@ class originatorImporter:
         queryList.append(query)
         print('sql generated')
         return query
+
+    ## Сохранение в файл
+
+    def export(self, provider):
+        self.exportCsvFile(provider)
+        self.exportSimpleSqlFile(provider)
+        self.exportAllSqlFile(provider)
+        exit()
+    
+    def exportCsvFile(self, provider):
+        fileName = 'originators_' + provider + '_registrations.csv'
+        csvUnload(self.outputListDict(), fileName, [
+            'provider',
+            'operator_group_id', 
+            'service_type_id', 
+            'originator_change', 
+            'originator', 
+            'contractor_legal_entity', 
+            'contractor_inn', 
+            'is_incorrect_inn', 
+            'status_id',
+            'account_id'
+        ])
+        print(fileName + ' saved')
+
+        '''
+        ## Генерация запроса по операторам на разные файлы
+        for key in list(Originators.OriginatorsByOperatorsGropupId.keys()):
+            queryList = Originators.sqlGenerate(Originators.OriginatorsByOperatorsGropupId.get(key), 20000)
+            fileCount = int()
+            for query in queryList:
+                fileCount += 1
+                f = open('./output/originators_smsc_' + key + '_' + str(fileCount) + '.sql', 'w', encoding='utf8')
+                f.write(query)
+        '''
+
+        ## Генерация запроса в один файл
+    def exportSimpleSqlFile(self, provider):
+        queryString = self.sqlGenerate(self.Originators, 20000, provider)
+        queryString = re.sub(r'\n+\s+', ' ', queryString)
+        queryString = re.sub(r'\s+\n+', ' ', queryString)
+        queryString = re.sub(r'\n+', ' ', queryString)
+        queryString = re.sub(r'\s', ' ', queryString)
+
+        fileName = 'originators_' + provider + '.sql'
+        f = open('./output/' + fileName, 'w', encoding='utf8')
+        f.write(queryString)
+        print(fileName + ' saved')
+
+        ## Генерация запроса написания имен
+    def exportAllSqlFile(self, provider):
+        querySpellingString = self.spellingOriginatorsSqlGenerate()
+        querySpellingString = re.sub(r'\n+\s+', ' ', querySpellingString)
+        querySpellingString = re.sub(r'\s+\n+', ' ', querySpellingString)
+        querySpellingString = re.sub(r'\n+', ' ', querySpellingString)
+        querySpellingString = re.sub(r'\s', ' ', querySpellingString)
+
+        fileName = 'spelling_originators_' + provider + '.sql'
+        f = open('./output/' + fileName, 'w', encoding='utf8')
+        f.write(querySpellingString)
+        print(fileName + ' saved')
